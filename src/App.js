c83b77955742e82d81b3b23e2b2cd3cb6c336db7
Your Name <108307350+pjduri@users.noreply.github.com>
2023-08-19 03:58:31 -0500

    added click handler with console log

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css'
 
-const Square = ({val}) => <button className='square'>{val}</button>
+const a = console.log.bind()
+
+const Square = ({ val }) => {
+  const handleClick = () => { a('clicked!') }
+
+  return <button className='square' onClick={handleClick}>{val}</button>
+}
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        {[1,2,3].map(r => <Square val={r} />)}
+        {[1, 2, 3].map(r => <Square val={r} />)}
       </div>
       <div className="board-row">
-        {[4,5,6].map(r => <Square val={r} />)}
+        {[4, 5, 6].map(r => <Square val={r} />)}
       </div>
       <div className="board-row">
-        {[7,8,9].map(r => <Square val={r} />)}
+        {[7, 8, 9].map(r => <Square val={r} />)}
       </div>
     </>
   )
