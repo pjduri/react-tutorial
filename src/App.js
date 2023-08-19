c10c3486021d32af151da3babde7d9a06ce9a522
Your Name <108307350+pjduri@users.noreply.github.com>
2023-08-19 03:56:48 -0500

    added square component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css'
 
+const Square = ({val}) => <button className='square'>{val}</button>
+
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        {[1, 2, 3].map(r => <button className='square'>{r}</button>)}
+        {[1,2,3].map(r => <Square val={r} />)}
       </div>
       <div className="board-row">
-        {[4,5,6].map(r => <button className='square'>{r}</button>)}
+        {[4,5,6].map(r => <Square val={r} />)}
       </div>
       <div className="board-row">
-        {[7,8,9].map(r => <button className='square'>{r}</button>)}
+        {[7,8,9].map(r => <Square val={r} />)}
       </div>
     </>
   )
