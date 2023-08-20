33c796b84d73a4f94dcc559594f891b41af245a5
Your Name <108307350+pjduri@users.noreply.github.com>
2023-08-20 13:33:07 -0500

    added helper function to calculate winner

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function Board() {
   const [ xIsNext, setXIsNext ] = useState(true)
   const [ squares, setSquares ] = useState(Array(9).fill(null))
   const handleClick = (i) => {
-    if (squares[i]) return 
+    if (squares[i] || calculateWinner(squares)) return 
     const nextSquares = squares.slice()
     nextSquares[i] = xIsNext ? 'X' : 'O'
     setSquares(nextSquares)
@@ -31,3 +31,21 @@ export default function Board() {
   )
 }
 
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (const [a,b,c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
