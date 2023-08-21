91ee3fb47eab88bac2e0e36c3394af25ddd6826f
Your Name <108307350+pjduri@users.noreply.github.com>
2023-08-20 22:17:25 -0500

    finished jumpTo with setCurrentMove and tied xIsNext to current move number

--- src/App.js
+++ src/App.js
@@ -38,17 +38,19 @@ const Board = ({ xIsNext, squares, onPlay }) => {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true)
-  const [history, setHistory] = useState([Array(9).fill(null)])
-  const currentSquares = history[history.length - 1]
+  const [ history, setHistory ] = useState([Array(9).fill(null)])
+  const [ currentMove, setCurrentMove ] = useState(0)
+  const currentSquares = history[currentMove]
+  const xIsNext = currentMove % 2 === 0
 
   const handlePlay = (nextSquares) => {
-    setHistory([...history, nextSquares])
-    setXIsNext(!xIsNext)
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1)
   }
 
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove)
   }
 
   const moves = history.map((squares, move) =>
@@ -72,7 +74,6 @@ export default function Game() {
     </div>
   )
 }
-
 
 const calculateWinner = (squares) => {
   const lines = [
