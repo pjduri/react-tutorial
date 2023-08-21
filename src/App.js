10cabe01106d8ae7abd6c7822a93128e4f1c2d04
Your Name <108307350+pjduri@users.noreply.github.com>
2023-08-21 00:40:50 -0500

    extra practice 4 - highlight winning squares

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css'
 import { useState } from 'react'
 
-const Square = ({ value, onSquareClick }) =>
-  <button className='square' onClick={onSquareClick}>{value}</button>
+const Square = ({ value, onSquareClick, isWinner }) =>
+  <button className='square' onClick={onSquareClick} style={isWinner ? { backgroundColor: 'red' } : {}}>
+    {value}
+  </button>
 
 const Board = ({ xIsNext, squares, onPlay }) => {
   const winner = calculateWinner(squares)
-  const winHeader = <h1>{winner ? `Winner: ${winner}` : `Next Player: ${xIsNext ? 'X' : 'O'}`}</h1>
+  const winHeader = <h1>{winner ? `Winner: ${winner[0]}` : `Next Player: ${xIsNext ? 'X' : 'O'}`}</h1>
   const tieHeader = <h1>TIE: No more moves.</h1>
   const rows = [squares.slice(0, 3), squares.slice(3, 6), squares.slice(6)]
 
   const handleClick = (i) => {
-    if (squares[i] || calculateWinner(squares)) return
+    if (squares[i] || winner) return
     const nextSquares = squares.slice()
     nextSquares[i] = xIsNext ? 'X' : 'O'
     onPlay(nextSquares)
@@ -25,7 +27,11 @@ const Board = ({ xIsNext, squares, onPlay }) => {
       {rows.map((r, i) =>
         <div key={i} className="board-row">
           {r.map((s, j) =>
-            <Square key={j} value={s} onSquareClick={() => handleClick(j + i * 3)} />)}
+            <Square key={j}
+              value={s}
+              onSquareClick={() => handleClick(j + i * 3)}
+              isWinner={winner && winner[1].includes(j + i * 3)}
+            />)}
         </div>
       )}
     </>
@@ -86,7 +92,7 @@ const calculateWinner = (squares) => {
   ]
   for (const [a, b, c] of lines) {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
+      return [squares[a], [a, b, c]]
     }
   }
   return null
